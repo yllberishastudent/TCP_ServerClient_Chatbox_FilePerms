1db0b4ecd9c6af88aafc386969c57a718cd875f9
Vlera-Tahiri <tahirivlere@gmail.com>
2022-04-10 16:09:29 +0200

    +

---
+++ Socket_Programming/src/sockets/programming/Client.java
@@ -0,0 +1,77 @@
+package sockets.programming;
+
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Client implements Runnable {
+
+	private Socket client;
+	private BufferedReader in;
+	private PrintWriter out;
+	private boolean done;
+
+	@Override
+	public void run() {
+		try {
+			Socket client = new Socket("localhost", 9999);
+			out = new PrintWriter(client.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+			InputHandler inHandler = new InputHandler();
+			Thread t = new Thread(inHandler);
+			t.start();
+
+			String inMessage;
+			while ((inMessage = in.readLine()) != null) {
+				System.out.println(inMessage);
+			}
+
+		} catch (IOException e) {
+			shutdown();
+		}
+	}
+
+	public void shutdown() {
+		done = true;
+		try {
+			in.close();
+			out.close();
+			if (!client.isClosed())
+				client.close();
+		} catch (IOException e) {
+		}
+	}
+
+	class InputHandler implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+				BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+				while (!done) {
+					String message = inReader.readLine();
+					if (message.equals("/quit")) {
+						out.println(message);
+						inReader.close();
+						shutdown();
+					} else {
+						out.println(message);
+					}
+				}
+			} catch (Exception e) {
+				shutdown();
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.run();
+	}
+}
